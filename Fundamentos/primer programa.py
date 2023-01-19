2f73465d91095c5944e3c0bd66657715050742f8
savendanog <emprendesym@gmail.com>
2023-01-19 18:02:50 -0500

    Función 'print' y comentarios en python

---
+++ Fundamentos/primer programa.py
@@ -0,0 +1,20 @@
+print("Hola, esto es Python") #La función print me imprime en consola el mensaje que ponga dentro de las comillas
+
+print("Hola, soy Sebastian y tengo 30 años")
+
+#También puedo usar la función print para ejecutar operaciones matemáticas e imprimirlas en consola:
+print(12 + 5)
+print(10 -5)
+print(2 * 3)
+print(8 / 2)
+
+#Para un comentario de varias líneas puedar usar comillas triples, así:
+"""
+Esto es un comentario
+que ocupa
+varias líneas    
+"""
+'''
+También se pueden
+usar las comillas simples
+'''
